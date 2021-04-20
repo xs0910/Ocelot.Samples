d35c245ea8bbe60ec899989e23e82be30a0f552d
涛 <997933096@qq.com>
2021-04-20 19:14:17 +0800

    提交简单demo

---
+++ ServiceApi1/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Content($"Hello ServiceApi1----{Request.HttpContext.Connection.LocalPort}");
+        }
+    }
+}
